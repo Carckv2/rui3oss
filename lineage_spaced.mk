189add636e4cce21fa455a6cad0e0f7fa310f1db
“Carckv2” <Ankitshresthaas2@gmail.com>
2023-05-22 05:36:10 -0400

    initial-tree

---
+++ lineage_spaced.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/realme/spaced/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_spaced
+PRODUCT_DEVICE := spaced
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme 8i
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=spaced \
+    PRODUCT_NAME=spaced \
+    PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1670487331102 release-keys"
+
+BUILD_FINGERPRINT := "realme/RMX3151/RE54B4L1:12/SP1A.210812.016/1670487331102:user/release-keys"
